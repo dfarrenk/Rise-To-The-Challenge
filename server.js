bdd5173846305a6387a843e5bb7ade7bb19eeaea
Pat hayes <pat2382@yahoo.com>
2017-12-20 17:40:35 +0000

    mvc set up

---
+++ server.js
@@ -0,0 +1,28 @@
+var express=require("express");
+var bodyParser=require("body-parser");
+var methodOverride=require("method-override");
+var exphbs=require("express-handlebars");
+
+var PORT=process.env.PORT || 8080;
+var app=express();
+var db = require("./models");
+
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(bodyParser.text()); 
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+require("./controller/challenge-controller.js")(app);
+
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
