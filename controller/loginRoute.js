0cbab1a977e5a8a9a834714b384dd25a4d43d046
71emj <tim.jeng@gmail.com>
2017-12-20 13:46:53 -0500

    basic signin

--- controller/loginRoute.js
+++ controller/loginRoute.js
@@ -7,19 +7,26 @@ module.exports = function() {
 	const dataBase = require("../models");
 		loginRoute = require("express").Router();
 
-
 	loginRoute.get("/login", function(req, res) {
 		console.log("sign in with username/pass");
 		res.send(200).send("login");
 	});
 
 	loginRoute.post("/login", function(req, res) {
-		
-
-		
+		dataBase.User.findOne({
+			where: {
+				name: req.body.username
+			}
+		}).then((data) => {
+			if (!data) {
+				return res.status(404).send("user name not exist");
+			}
+			if (data.password !== req.body.password) {
+				return res.status(401).send("wrong password");
+			}
+			res.status(200).json("placeholder: send session token");
+		});
 	});
-
-
 
 	return loginRoute;
 }
