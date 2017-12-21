8943fe8983d1ef61ffb6cd21c6e531de71d67af0
71emj <tim.jeng@gmail.com>
2017-12-21 11:16:56 -0500

    implement passport

--- controller/loginRoute.js
+++ controller/loginRoute.js
@@ -6,6 +6,7 @@ module.exports = function() {
 
    // add passport 
    const dataBase = require("../models"),
+      passport = require("../config/local.js"),
       loginRoute = new require("express").Router();
 
    // move to html route
@@ -15,63 +16,72 @@ module.exports = function() {
    });
 
    loginRoute.post("/newaccount", function(req, res) {
-   	console.log(req.body);
+      console.log(req.body);
 
-   	dataBase.User.create({
-   		name: req.body.username,
-   		password: req.body.password,
-   		alias: req.body.alias || req.body.username,
-   		email: req.body.email
-   	}).then(() => {
-   		res.status(201).send("Registered..please verify your email address");
-   	}).catch((err) => {
-   		const errorType = err.errors[0].message,
-   			errorcode = errorHandler(errorType);
+      dataBase.User.create({
+         name: req.body.username,
+         password: req.body.password,
+         alias: req.body.alias || req.body.username,
+         email: req.body.email
+      }).then(() => {
+         res.status(201).send("Registered..please verify your email address");
+      }).catch((err) => {
+         const errorType = err.errors[0].message,
+            errorcode = errorIdentifier(errorType);
 
-   		switch(errorcode) {
-   			case 1:
-   				return res.status(409).send("Username taken");
-   				break;
-   			case 2: 
-   				return res.status(406).send("Invalid password format");
-   				break;
-   			case -1:
-   				return res.status(400).send("Bad request");
-   				break;
-   		}
-   	});
+         switch (errorcode) {
+            case 1:
+               return res.status(409).send("Username taken");
+               break;
+            case 2:
+               return res.status(406).send("Invalid password format");
+               break;
+            case -1:
+               return res.status(400).send("Bad request");
+               break;
+         }
+      });
    });
 
-   loginRoute.post("/login", function(req, res) {
-      console.log(req.body);
-      dataBase.User.findOne({
-         where: {
-            name: req.body.username
-         }
-      }).then((data) => {
-         if (!data) {
-            return res.status(404).send("user name doesn't exist");
-         }
-         if (data.password !== req.body.password) {
-            return res.status(401).send("wrong password");
-         }
-         res.status(200).json("placeholder: send session token");
-      });
+   // loginRoute.post("/login", function(req, res) {
+   //    console.log(req.body);
+   //    dataBase.User.findOne({
+   //       where: {
+   //          name: req.body.username
+   //       }
+   //    }).then((data) => {
+   //       if (!data) {
+   //          return res.status(404).send("user name doesn't exist");
+   //       }
+   //       if (data.password !== req.body.password) {
+   //          return res.status(401).send("wrong password");
+   //       }
+   //       res.status(200).json("placeholder: send session token");
+   //    });
+   // });
+
+   loginRoute.post("/login", passport.authenticate("local", {
+      successRedirect: "/user/dashboard",
+      failureRedirect: "/",
+      failureFlash: false
+   }), function(req, res) {
+      console.log("success");
+      res.status(200).send("success");
    });
 
    return loginRoute;
 }
 
-function errorHandler(errtype) {
-	console.error(errtype);
-	// identify error type using regex to match key word
-	if (!!errtype.match(/\w*(?:name)/g)) {
-		return 1;
-	}
+function errorIdentifier(errtype) {
+   console.error(errtype);
+   // identify error type using regex to match key word
+   if (!!errtype.match(/\w*(?:name)/g)) {
+      return 1;
+   }
 
-	if (!!errtype.match(/\w*(?:password)/g)) {
-		return 2;
-	}
+   if (!!errtype.match(/\w*(?:password)/g)) {
+      return 2;
+   }
 
-	return -1;
+   return -1;
 }
--- server.js
+++ server.js
@@ -2,26 +2,35 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // var methodOverride=require("method-override");
 var exphbs = require("express-handlebars");
+var Exsess = require("express-session");
+var CookieParser = require("cookie-parser")
 
 var PORT = process.env.PORT || 8080;
 var app = express();
 var db = require("./models");
+var passport = require("./config/local.js");
+var errorHandler = require(Path.join("./errorhandler/handler.js"));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static("public"));
+app.use(CookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(Exsess({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(function(err, req, res, next) {
    if (err) {
-   	console.error(err.stack);
-   	return res.status(500).send("Internal server error");	
+      console.error(err.stack);
+      return errorHandler(err);
    }
    next();
 });
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./controller/challenge-controller.js")(app);
 require("./controller/html-controller.js")(app);
@@ -33,17 +42,17 @@ app.post("/login", require("./controller/loginRoute.js")());
 app.post("/newaccount", require("./controller/loginRoute.js")());
 
 db.sequelize.sync({ force: true }).then(function() {
-  	// temp for testing only
+   // temp for testing only
    db.User.create({
       name: "71emj",
       password: "!wtuce40B65",
       alias: "71emj",
       email: "tim.jeng@gmail.com"
    }).catch((err) => {
-   	console.log(err);
+      console.log(err);
    });
 
    app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
    });
-});	
+});
