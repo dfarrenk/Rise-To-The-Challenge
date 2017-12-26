c1ddf248baddd363034a89291b024b0fc95fe8df
David Farrenkopf <dfarrenk@gmail.com>
2017-12-26 16:25:33 +0000

    User name field is now unique

--- models/user.js
+++ models/user.js
@@ -1,46 +1,36 @@
 // Model of the User from the challenge db.
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-            name: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            password: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            alias: {
-                type: DataTypes.STRING,
-                allowNull: true
-            },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                // validate: {
-                //     isEmail: true
-                // }
-
-            },
-            email_verified: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: false
-            }
-
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        alias: { // Not used for now
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // validate: {
+            //     isEmail: true
+            // }
+        },
+        email_verified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
         }
-        // TODO Hash password
-        // , {
-        //     hooks: {
-        //         aferValidate: function(user) {
-        //             user.password = bcrypt.hashSync(user.password, 8);
-        //         }
-        //     }
-        // }
-    );
+
+    });
 
     User.associate = function(models) {
         User.belongsToMany(models.Template, { through: { model: models.Instance, unique: false }, foreignKey: 'issuer_id' });
     };
-
 
     return User;
 };
